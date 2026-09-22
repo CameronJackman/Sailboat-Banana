65ca8d8abed46618b82c87725e91ea822a3736bc
CameronJackman <cameronjackman55@gmail.com>
2026-09-22 17:40:35 -0400

    Added Better Matching
    Added disappearing tokens on match
    Added Refill
    Added gravity effect

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections.Generic;
+using Unity.Mathematics;
 
 public class BoardManager : MonoBehaviour
 {
@@ -70,7 +71,7 @@ public class BoardManager : MonoBehaviour
     //gets a random token type when creating a piece
     private TokenType GetRandomToken()
     {
-        int randomIndex = Random.Range(0, 6);
+        int randomIndex = UnityEngine.Random.Range(0, 6);
 
         return (TokenType)randomIndex;
     }
@@ -82,11 +83,11 @@ public class BoardManager : MonoBehaviour
         List<Piece> matches =
             matchDetector.FindMatches(board, width, height);
 
-        Debug.Log("Matches found: " + matches.Count);
+        //Debug.Log("Matches found: " + matches.Count);
 
         foreach (Piece piece in matches)
         {
-            Debug.Log( "Match at: " + piece.gridposition + " - " + piece.TokenType);
+            //Debug.Log( "Match at: " + piece.gridposition + " - " + piece.TokenType);
 
         }
     }
@@ -107,21 +108,131 @@ public class BoardManager : MonoBehaviour
 
     public void TrySwap(Vector2Int firstPosition, Vector2Int secondPosition)
     {
+        //checks if the piece swapped is inside the game grid
         if(!IsInsideBoard(firstPosition) || !IsInsideBoard(secondPosition))
         {
             return;
         }
 
-        Piece firstPiece = board[firstPosition.x, firstPosition.y];
 
+        //making sure the two positions are next to each other 
+        int distnace = Mathf.Abs(firstPosition.x - secondPosition.x) + Mathf.Abs(firstPosition.y - secondPosition.y);
+
+        //if piece isnt 1 piece away the program will not swap
+        if (distnace != 1)
+        {
+            return;
+        }
+
+
+        //after pass checks put the swaping pieces into proper variables
+        Piece firstPiece = board[firstPosition.x, firstPosition.y];
         Piece secondPiece = board[secondPosition.x, secondPosition.y];
 
+        //check if its null so no null selection is passed breaking the game
         if (firstPiece == null || secondPiece == null)
+        {
             return;
-
+        }
+           
+        //performs the swaps
         SwapPieces(firstPosition, secondPosition);
+
+        //Check if swapping created a line up of 3 or more or a match
+        List<Piece> matches = matchDetector.FindMatches(board, width, height);
+
+        if (matches.Count > 0)
+        {
+            Debug.Log("Valid swap");
+
+            DestroyMatches(matches);
+        }
+        else
+        {
+            Debug.Log("Invalid Swap, (swap pieces back)");
+
+            //no match therefore return the swap
+            SwapPieces(firstPosition, secondPosition);
+        }
     }
 
+    //destroy any current matches
+    private void DestroyMatches(List<Piece> matches)
+    {
+        foreach (Piece piece in matches)
+        {
+            Vector2Int position = piece.gridposition;
+
+            //remove piece from array
+            board[position.x, position.y] = null;
+
+            //Destroy gameobj
+            Destroy(piece.gameObject);
+        }
+
+        CollapseBoard();
+        RefillBoard();
+
+        CheckForAfterMatches();
+    }
+
+    //Check for matches that are made after a match that was made 
+    private void CheckForAfterMatches()
+    {
+        List<Piece> matches = matchDetector.FindMatches(board, width, height);
+
+        if (matches.Count > 0)
+        {
+            DestroyMatches(matches );
+        }
+    }
+
+
+    //this adds more candy to baord after any matches
+    private void CollapseBoard()
+    {
+        for (int x = 0; x <width; x ++)
+        {
+            int emptySpaces = 0;
+
+            for (int y = 0; y <height; y++)
+            {
+                if (board[x, y] == null)
+                {
+                    emptySpaces++;
+                }
+                else if (emptySpaces > 0)
+                {
+                    //move piece down
+                    Piece piece = board[x, y];
+
+                    int newY = y- emptySpaces;
+
+                    board[x, newY] = piece;
+                    board[x, y] = null;
+
+                    piece.gridposition = new Vector2Int(x, newY);
+
+                    piece.transform.position = new Vector3(x, newY, 0);
+                }
+            }
+        }
+    }
+
+    //refill the board once matches are made and pieces are removed
+    private void RefillBoard()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y <height; y++)
+            {
+                if(board[x, y] == null)
+                {
+                    CreatePiece(x, y);
+                }
+            }
+        }
+    }
 
     private void SwapPieces(Vector2Int firstPosition, Vector2Int secondPosition)
     {
